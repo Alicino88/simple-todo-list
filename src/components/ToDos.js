7b5c0810709f7b8b025266e106f33f8540f8d380
Alice Moretti <ali.moret88@gmail.com>
2022-08-26 11:49:27 +0300

    todos are filtered according to category

---
+++ src/components/ToDos.js
@@ -0,0 +1,33 @@
+import FilterCategory from "./FilterCategory";
+import { useState } from "react";
+
+function ToDos(props) {
+  const [selectedCategory, setSelectedCategory] = useState("Study");
+  /*the category arrives from the child component FilterCategory */
+  const filterChangeCategory = (category) => {
+    console.log(category);
+    setSelectedCategory(category);
+  };
+
+  /*below we create a new array of todos filtered according to the current selected category */
+  const filteredToDos = props.items.filter((toDo) => {
+    return toDo.category === selectedCategory;
+  });
+  return (
+    <div>
+      <FilterCategory
+        onSelectCategory={filterChangeCategory}
+        visibleCategory={selectedCategory}
+      />
+      {filteredToDos.map((item) => (
+        <div key={item.key}>
+          <h3>{item.title}</h3>
+          <p>{item.hours}</p>
+          <p>{item.category}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ToDos;
