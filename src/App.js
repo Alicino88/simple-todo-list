744b0211e7d5ccc398f5c55bcb5eb56d3fa6f6cd
Alice Moretti <ali.moret88@gmail.com>
2022-08-26 11:49:46 +0300

    App component displays form and todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import NewToDo from "./components/NewToDo";
+import ToDos from "./components/ToDos";
+import { useState } from "react";
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  /*the data come from ToDoForm component when the "add task" button is clicked", passing through NewToDo */
+  const getData = (data) => {
+    /*previousData is initially an empty array that automatically comes as a argument (name is arbitrary) 
+    /*the todos are stored inside a state so to trigger a re-render of the component whenever we add a new todo*/
+    setToDos((previousData) => {
+      return [data, ...previousData];
+    });
+  };
+
+  /*if we would use the below syntax, at every re-render we would loose the previous toDo.
+  Therefore we need to use the spred operator
+  const getData = (data) => {
+  setToDos([data]);
+}; */
+  console.log(toDos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewToDo onSubmitDataAgain={getData} />
+      <ToDos items={toDos} />
     </div>
   );
 }
